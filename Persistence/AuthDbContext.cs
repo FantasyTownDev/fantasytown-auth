c61dbdd38b265ce1c520f5da125c786522994f4c
LitenPanJun <xiaopanjun233@163.com>
2026-09-14 12:43:27 +0800

    feat(db): add AuthDbContext with entity configurations

---
+++ Persistence/AuthDbContext.cs
@@ -0,0 +1,82 @@
+using FantasyTown.Auth.Modules.Accounts.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FantasyTown.Auth.Persistence;
+
+public class AuthDbContext : DbContext
+{
+    public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Player> Players => Set<Player>();
+    public DbSet<PlayerBan> PlayerBans => Set<PlayerBan>();
+    public DbSet<AuthLog> AuthLogs => Set<AuthLog>();
+    public DbSet<Report> Reports => Set<Report>();
+    public DbSet<Notification> Notifications => Set<Notification>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Uid);
+            entity.HasIndex(e => e.Email).IsUnique();
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Ip).HasMaxLength(45);
+            entity.Property(e => e.BannedReason).HasMaxLength(255);
+            entity.Property(e => e.SecurityStamp).HasMaxLength(64);
+            entity.HasQueryFilter(e => !e.IsDeleted);
+        });
+        
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.HasKey(e => e.Pid);
+            entity.HasIndex(e => e.Name).IsUnique();
+            entity.HasIndex(e => e.Uuid).IsUnique();
+            entity.HasIndex(e => e.Uid).IsUnique();
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Uuid).HasMaxLength(32);
+            entity.HasOne(e => e.User)
+                .WithOne(u => u.Player)
+                .HasForeignKey<Player>(e => e.Uid)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+        
+        modelBuilder.Entity<PlayerBan>(entity =>
+        {
+            entity.HasKey(e => e.BanId);
+            entity.HasIndex(e => e.Pid);
+            entity.Property(e => e.BannedReason).HasMaxLength(255);
+            entity.HasOne(e => e.Player)
+                .WithMany(p => p.PlayerBans)
+                .HasForeignKey(e => e.Pid)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+        
+        modelBuilder.Entity<AuthLog>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.SubjectId);
+            entity.HasIndex(e => e.CreatedAt);
+            entity.Property(e => e.Action).HasMaxLength(50);
+            entity.Property(e => e.Ip).HasMaxLength(45);
+        });
+        
+        modelBuilder.Entity<Report>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.TargetType).HasMaxLength(20);
+        });
+        
+        modelBuilder.Entity<Notification>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Type).HasMaxLength(50);
+            entity.Property(e => e.NotifiableType).HasMaxLength(50);
+        });
+    }
+}
